8615a0611ea5bcef091c86f7d629d198ec6b1da0
Pablo <pablogcg05@gmail.com>
2023-04-26 13:31:52 +0200

    Cambios finales en código

--- Program.cs
+++ Program.cs
@@ -1,29 +1,27 @@
 ﻿Console.WriteLine("Hola, el siguiente prgrama consistirá en que usted adivine un número entre 1 y 15");
 Random rnd = new Random();
 int numero = rnd.Next(1,15);
-int intentos = 0;
+int intentos = 2;
 
 Console.WriteLine("Introduzca un número del 1 al 15");
-intentos = Convert.ToInt32(Console.ReadLine());
-while (intentos != numero && intentos <= 3)
+int numeroUsuario = Convert.ToInt32(Console.ReadLine());
+
+while (intentos > 0)
 {
-    if(intentos < numero)
+    if (numeroUsuario == numero)
     {
-        Console.WriteLine("El numero es mayor que el que has puesto");
+    Console.WriteLine("Has ganado");
+    }
+    else if(numero < numeroUsuario)
+    {
+        Console.WriteLine("El numero es menor que el que ha puesto");
     }
     else
     {
-        Console.WriteLine("El numero es menor que el que has puesto");
+        Console.WriteLine("El numero es mayor que el que ha puesto");
     }
+    intentos--;
     Console.WriteLine("Intentalo otra vez");
-    intentos = Convert.ToInt32(Console.ReadLine());
+    numeroUsuario = Convert.ToInt32(Console.ReadLine());
 }
-
-if (intentos == numero)
-{
-    Console.WriteLine("Has ganado");
-}
-else
-{
-    Console.WriteLine("Has perdido");
-}
+Console.WriteLine("Has perdido");
